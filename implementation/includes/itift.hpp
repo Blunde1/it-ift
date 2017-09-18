0afea291fb730931d5751c6c0caebf946539b39b
Blunde1 <blu012@webmail.uib.no>
2017-09-18 19:07:19 +0200

    added Hermite IFT

--- implementation/includes/itift.hpp
+++ implementation/includes/itift.hpp
@@ -6,6 +6,7 @@
 /*#include "includes/optimization.hpp"*/
 #include "complex.hpp" 
 #include "quadrature_integration.hpp"
+#include "Fourier_inversion.hpp"
 /*#include "includes/differentials.hpp" 
 #include "includes/generating_functions.hpp"
 #include "includes/saddlepoint_approximation.hpp"
