4f0042a21fe33294469fa5662086c79b18a0af7d
Blunde1 <blu012@webmail.uib.no>
2017-09-18 19:09:48 +0200

    added all structs and helper functions, only obj fun left

--- implementation/generic_itift_tmp.cpp
+++ implementation/generic_itift_tmp.cpp
@@ -7,11 +7,11 @@
 #include<TMB.hpp>
 
 /* include cType */
-#include "../../cpp_math_library/includes/complex.hpp"
+#include "includes/itift.hpp"
 
 /* differentials diffusion */
 template<class Type>
-vector<Type> differentials_diff(Type x0, Type dt, vector<Type> p, int process_type){
+vector<Type> differentials_diff(Type x0, Type dt, vector<Type> p, vector<Type> p_jump, int process_type){
     
     /**
      * Returns derivatives of order 0-2 w.r.t. x0 for the drift and diffusion 
@@ -35,14 +35,14 @@ vector<Type> differentials_diff(Type x0, Type dt, vector<Type> p, int process_ty
     {
     case 1: 
         /* Geometric Brownian Motion */
-        kappa = par[0], sigma = par[1];
+        kappa = p[0], sigma = p[1];
         drift = kappa * x0, diffusion = sigma*x0;
         drift_1 = kappa, diffusion_1 = sigma;
         drift_2 = 0, diffusion_2 = 0;
         break;
     case 2:
         /* log GBM */
-        kappa = par[0], sigma=par[1];
+        kappa = p[0], sigma=p[1];
         drift = kappa - 0.5*sigma*sigma;
         drift_1 = 0, drift_2 = 0;
         diffusion = sigma;
@@ -58,17 +58,92 @@ vector<Type> differentials_diff(Type x0, Type dt, vector<Type> p, int process_ty
 /* differentials jd */
 
 /* log_cf function */
+template<class Type>
+cType<Type> log_cf_fun(cType<Type> s, Type x0, Type dt, vector<Type> p, vector<Type> p_jump, int process_type, int scheme, int jump){
 
+    // Differentials
+    vector<Type> deriv = differentials_diff(x0, dt, p, p_jump, process_type);
+    Type drift, drift_1, drift_2, diffusion, diffusion_1, diffusion_2;
+    drift = deriv[0], drift_1=deriv[1], drift_2=deriv[2];
+    diffusion=deriv[3], diffusion_1=deriv[4], diffusion_2=deriv[5];
+    
+    // Jump parameters
+    Type lambda, mu, nu;
+    
+    //Characteristic function of schemes from - PW-2012 
+    cType<Type> lcf, lcf_RS;
+    Type c_1, c_2, c_3, c_4;
+    cType<Type> i(0,1);
+    switch (scheme) {
+    case 1: 
+        /* Euler-Maruyama */
+        lcf = (s*i * (x0 + drift*dt) - pow(s * diffusion, (Type)2)*dt / (Type)2);
+        break;
+    case 2:
+        /* Milstein */
+        c_1=diffusion, c_2=0.5*diffusion*diffusion_1, c_3=dt*(drift-c_2);
+        lcf_RS = (-c_1*c_1*s*s*dt) / ((Type)2 - ((Type)4)*i*s*c_2*dt  );// -
+        //((Type)0.5)*log((Type)1 - ((Type)2)*dt*c_2*s*i);
+        lcf = lcf_RS + (i*s*(x0+c_3));
+        break;
+    case 3:
+        /* Scheme 3 */
+        c_1 = diffusion + (drift*diffusion_1 + 0.5*pow(diffusion,2)*diffusion_2)*dt;
+        c_2 = 0.5*diffusion*diffusion_1;
+        c_3 = diffusion*drift_1 - drift*diffusion_1 - 0.5*pow(diffusion, 2)*diffusion_2;
+        c_4 = (drift - 0.5*diffusion*diffusion_1)*dt + 
+            (drift*drift_1 + 0.5*pow(diffusion, 2)*drift_2)*0.5*pow(dt, 2);
+        lcf_RS = -dt*s*s*(((Type)6)*c_1*c_1 + ((Type)6)*c_1*c_3*dt + ((Type)2)*c_3*c_3*dt*dt - i*s*c_3*c_3*dt*dt*dt*c_2) / ((Type)12 - 
+            ((Type)24)*i*s*c_2*dt) - ((Type)0.5)*log((Type)1 - ((Type)2)*dt*c_2*s*i);
+        lcf = lcf_RS + (i*s*(x0+c_4));
+        break;
+    }
+    
+    // Adding jumps
+    switch(jump){
+    case 0:
+        lcf = lcf; // Nothing happens
+        break;
+    case 1: // Normally distirbuted jumps
+        lambda = p_jump[0], mu = p_jump[1], nu = p_jump[2];
+        lcf = lcf + lambda*dt*(exp(s*i*mu - ((Type)0.5)*nu*nu*s*s)- (Type)1);
+        break;
+    case 2: // Gamma distributed jumps
+        lambda = p_jump[0], mu = p_jump[1], nu = p_jump[2];
+        // mu>0: shape, nu>0: scale
+        lcf = lcf + lambda*dt*(pow(((Type)1 - nu*i*s), -mu) - (Type)1);
+        break;
+    }
+    
+    return lcf;
+}
 
 /* 1. log_cf struct */
 /* 1. 1. IFT - GH */
 /* 1. 2. IFT - GL */
+template<class Type>
+struct log_cf{
+    //cType<Type>& s;
+    Type x0, dt;
+    vector<Type> p, p_jump;
+    int process, scheme, jump;
+    logcf_f(Type x0_, Type dt_, vector<Type> p_, vector<Type> p_jump_, int process_, int scheme_, int jump_) :
+        x0(x0_), dt(dt_), p(p_), p_jump(p_jump_), process(process_), scheme(scheme_), jump(jump_) {}
+    template <class T>
+    cType<T> operator()(const cType<T>& s, const T& x0) {
+        T dt_ = T(dt);
+        vector<T> p_ = p.template cast<T>();
+        vector<T> p_jump_ = p.template cast<T>();
+        cType<T> lcf = log_cf_fun(s, x0, dt_, p_, p_jump_, (int)process, (int)scheme, (int)jump);
+        //cType<Type> lcf = log_cf_fun(s, x0_, (Type)dt, (vector<Type>)p, (vector<Type>)p_jd (int)process, (int)scheme, (int)jump);
+        return lcf;
+    }
+};
 
 /* 2. log_mgf = cgf struct */
 /* 2. inner problem */
 /* 2. 1. newton optimiser */
 /* 2. 2.  */
-
 
 /* objective function */
 Type objective_function<Type>::operator()(){
