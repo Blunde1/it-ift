00d27fdef890ddb36e021b9a3d7c85e421c1128d
Blunde1 <blu012@webmail.uib.no>
2017-09-26 12:34:02 +0200

    Completed Fourier inversion methos

--- implementation/includes/Fourier_inversion.hpp
+++ implementation/includes/Fourier_inversion.hpp
@@ -2,17 +2,33 @@
 #ifndef __FOURIER_INVERSION_HPP_INCLUDED__
 #define __FOURIER_INVERSION_HPP_INCLUDED__
 
+/** Gauss hermite fourier transfomr from lcf to density. lcf takes s, ls0, v0, as argument
+ */
+template<class Type, class Functor>
+Type ift_gauher(Type x, Functor f, matrix<Type> rules){
+    cType<Type> i(0,1);
+    Type sum=0;
+    int n = (rules.rows()+1) / 2; // symmetric roots and real part of integrand about origin
+    vector<Type> ai = rules.col(0);
+    vector<Type> w = rules.col(1);
+    for(int j=0; j<n; j++){
+        sum += w[j]*real(exp(f(cType<Type>(ai[j]))  - i*ai[j]*x + ai[j]*ai[j] ));
+    }
+    sum = sum / ((Type)(M_PI));
+    return sum;
+}
+
 /** Gauss laguerre fourier transformation from logarithmich characteristic function to density function 
  */
 template<class Type, class Functor>
-Type logcf_to_dens(Type x, Type x0, Functor f, matrix<Type> quadRules){
+Type ift_gulag(Type x, Functor f, matrix<Type> quadRules){
     cType<Type> i(0,1);
     Type sum = 0; 
     int n = quadRules.rows();
     vector<Type> ai = quadRules.col(0);
     vector<Type> w = quadRules.col(1);
     for(int j=0; j<n; j++){
-        sum += w[j]*real(exp(f(cType<Type>(ai[j]),x0) - i*ai[j]*x+ai[j]));
+        sum += w[j]*real(exp(f(cType<Type>(ai[j])) - i*ai[j]*x+ai[j]));
     }
     sum = sum /((Type)M_PI);
     return sum;
@@ -21,35 +37,19 @@ Type logcf_to_dens(Type x, Type x0, Functor f, matrix<Type> quadRules){
 /** Gauss laguerre fourier transformation from logarithmich characteristic function to distribution function 
  */
 template<class Type, class Functor>
-Type logcf_to_dist(Type x, Type x0, Functor lcf, matrix<Type> quadRules){
+Type logcf_to_dist(Type x, Functor lcf, matrix<Type> quadRules){
     cType<Type> i(0,1);
     cType<Type> Fx(0);
     int n = quadRules.rows();
     vector<Type> ai = quadRules.col(0);
     vector<Type> w = quadRules.col(1);
     for(int j=0; j<n; j++){
-        Fx -= w[j]*(exp(lcf(cType<Type>(ai[j]),x0) - i*x*ai[j] + ai[j] ) - 
-            exp(lcf(cType<Type>(-ai[j]),x0) + i*x*ai[j] + ai[j] )) / (i*ai[j]);
+        Fx -= w[j]*(exp(lcf(cType<Type>(ai[j])) - i*x*ai[j] + ai[j] ) - 
+            exp(lcf(cType<Type>(-ai[j])) + i*x*ai[j] + ai[j] )) / (i*ai[j]);
     }
     Fx = Fx /((Type)2*M_PI);
     Fx += 0.5;
     return real(Fx);
-}
-
-/** Gauss hermite fourier transfomr from lcf to density. lcf takes s, ls0, v0, as argument
- */
-template<class Type, class Functor>
-Type ift_gauher(Type x, Type x0, Functor f, matrix<Type> rules){
-    cType<Type> i(0,1);
-    Type sum=0;
-    int n = (rules.rows()+1) / 2; // symmetric roots and real part of integrand about origin
-    vector<Type> ai = rules.col(0);
-    vector<Type> w = rules.col(1);
-    for(int j=0; j<n; j++){
-        sum += w[j]*real(exp(f(cType<Type>(ai[j]), x0)  - i*ai[j]*x + ai[j]*ai[j] ));
-    }
-    sum = sum / ((Type)(2*M_PI));
-    return sum;
 }
 
 
