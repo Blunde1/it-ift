ac00106a0027bd994497b29dfb8b0b3895f1d471
Blunde1 <blu012@webmail.uib.no>
2017-09-29 07:40:36 +0200

    improved code readability for exact spa via cgf

--- implementation/jd_ift_exact_spa.cpp
+++ implementation/jd_ift_exact_spa.cpp
@@ -7,30 +7,6 @@
 #include<TMB.hpp>
 #include "includes/itift.hpp"
 
-template<class Type>
-struct lcf_standardized{
-    spa_iprob<Type> iprob;
-    Type x;
-    vector<Type> sp; // saddlepoint
-    
-    lcf_standardized(spa_iprob<Type> iprob_, Type x_, vector<Type> sp_) : 
-        iprob(iprob_), x(x_), sp(sp_) {}
-    
-    template<typename T>
-    cType<T> operator()(cType<T> s){
-        T x_ = T(x);
-        vector<T> sp_ = sp.template cast<T>();
-        matrix<T> H = autodiff::hessian(iprob, sp_);
-        cType<T> res, i(0,1);
-        res = -(iprob(sp_)+sp_(0)*x_)  - i*s*x_ / sqrt(abs(H(0,0))) + 
-            iprob(
-                sp_(0) + s*i / sqrt(abs(H(0,0)))
-            )
-            + (sp_(0) + s*i / sqrt(abs(H(0,0))))*x_;
-        return res;
-    }
-};
-
 /* objective function */
 template<class Type>
 Type objective_function<Type>::operator()(){
@@ -42,7 +18,7 @@ Type objective_function<Type>::operator()(){
     DATA_INTEGER(scheme);
     DATA_INTEGER(jump);
     DATA_INTEGER(niter);
-    DATA_INTEGER(qhiter);
+    DATA_INTEGER(ghiter);
     
     // Parameters
     PARAMETER_VECTOR(par); // jump diffusion parameters
@@ -52,14 +28,20 @@ Type objective_function<Type>::operator()(){
     vector<Type> s(1), deriv(6);
     
     // Qaadrature rules    
-    matrix<Type> rules(qhiter,2);
+    matrix<Type> rules(ghiter,2);
     rules = gauss_hermite_quad(rules);
     
+    // Create cgf
+    cgf_s<Type> cgf(X(0), dt, par, process, scheme, jump);
+    
     // Create inner problem
-    spa_iprob<Type> iprob(X(1), X(0), dt, par, process, scheme, jump);
-
+    //spa_iprob<Type> iprob(X(1), X(0), dt, par, process, scheme, jump);
+    spa_iprob<Type> iprob(cgf, X(1));
+        
     // Build nll from spa
     for(j=1; j<nobs; j++){
+        // Update cgf
+        cgf.set_x0(X(j-1));
         // Update inner problem
         iprob.set_x0(X(j-1));
         iprob.set_x( X(j) );
@@ -67,14 +49,11 @@ Type objective_function<Type>::operator()(){
         deriv = differentials_diff(X(j-1), dt, par, process);
         s(0) = (X(j) - X(j-1) - deriv(0)*dt) / (deriv(3)*deriv(3)*dt); // start at spa for Normal approximation
         s(0) = newton_local_extrema(iprob, s, niter);
-        // Create standardized lcf with a get_value method
-        lcf_standardized<Type> lphi_01(iprob, X(j), s);
-        // Invert to obtain standardized density
-        fx_standardized = ift_gauher(Type(0), lphi_01, rules);
-        //REPORT(fx_standardized);
-        // Calculate exact spa
-        lfx = iprob(s) + log(fx_standardized) - 
-            Type(0.5)*atomic::logdet(autodiff::hessian(iprob,s));
+        // Create standardized lcf
+        lcf_standardized<Type> lphi_01(cgf, X(j), s);
+        // Calculate log exact spa
+        lfx = log_exact_spa(X(j), cgf, s, lphi_01, rules );
+        // Update likelihood
         nll -= lfx;
     }
     
